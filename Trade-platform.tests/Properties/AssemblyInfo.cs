56c9cda1d3d0f73b859b211a8963c37ac45090e7
Max Usanin <Usanin.Max@gmail.com>
2017-06-24 21:43:14 +0300

    version 3.0.0.0

--- Trade-platform.tests/Properties/AssemblyInfo.cs
+++ Trade-platform.tests/Properties/AssemblyInfo.cs
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [сборка: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
