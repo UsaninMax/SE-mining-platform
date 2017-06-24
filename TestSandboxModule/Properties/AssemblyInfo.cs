bccad55ae3382e547da84d6c040c82392556fd69
Max Usanin <Usanin.Max@gmail.com>
2017-06-24 21:35:11 +0300

    refactored

--- TestSandboxModule/Properties/AssemblyInfo.cs
+++ TestSandboxModule/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -8,9 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TestSandboxModule")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyProduct("TestSandboxModule")]
-[assembly: AssemblyCopyright("Copyright © Luxoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
